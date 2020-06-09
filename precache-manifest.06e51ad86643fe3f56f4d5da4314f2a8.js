2da8b6279ab7d34be42847393871505e382ef95b
pranav1812 <pranavvohra1812@yahoo.in>
2020-06-10 00:17:17 +0530

    Updates

---
+++ precache-manifest.06e51ad86643fe3f56f4d5da4314f2a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72009ca312b21ed31007f47c860b066f",
+    "url": "/Landing_Page/index.html"
+  },
+  {
+    "revision": "f7bd69afa8795d4bb07b",
+    "url": "/Landing_Page/static/js/2.2277d66d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Landing_Page/static/js/2.2277d66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33904db0cc58486f798",
+    "url": "/Landing_Page/static/js/main.f301dd0b.chunk.js"
+  },
+  {
+    "revision": "b84721293e33ad423c2a",
+    "url": "/Landing_Page/static/js/runtime-main.34b6e246.js"
+  },
+  {
+    "revision": "441eb3a6c67d6a4f3a465694d0f49d98",
+    "url": "/Landing_Page/static/media/mentorship_plant.441eb3a6.png"
+  },
+  {
+    "revision": "7a2771627738ca46cd5fe4232629c36d",
+    "url": "/Landing_Page/static/media/mtp.7a277162.png"
+  },
+  {
+    "revision": "61f15c3f4a3a271b02a5751902721569",
+    "url": "/Landing_Page/static/media/path.61f15c3f.png"
+  }
+]);
